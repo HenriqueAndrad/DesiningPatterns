c64c87ffe2ae46d3f17b80967f1dd65ad75a7703
Rick <henrique.gomes5989@gmail.com>
2019-05-10 11:08:29 -0300

    Só por deus irmão

---
+++ Assets/Scripts/Invoker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Invoker : MonoBehaviour
+{
+    private string m_ID;
+    public string ID { get { return m_ID; } }
+    private string m_Type;
+    public string Type { get { return m_Type; } }
+
+    private string[] m_Strings;
+    [SerializeField] private InputField m_InputField;
+
+
+    private void Update()
+    {
+       m_Strings = m_InputField.text.Split(' ');
+    }
+}
---
+++ Assets/Scripts/SetType.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Command
+{
+    public class SetType : MonoBehaviour, ICommand
+    {
+        [SerializeField] private string m_ID;
+        [SerializeField] private string m_Type;
+        [SerializeField] private GameObject m_Object;
+
+
+        public void Execute(Invoker invoker)
+        {
+            m_ID = invoker.ID;
+            m_Type = invoker.Type;
+            SetItemType();
+        }
+
+        public void SetItemType()
+        {
+            m_Object = GameObject.Find(m_ID);
+            m_Object.GetComponent<Item>().m_Type = m_Type;
+        }
+    }
+}
